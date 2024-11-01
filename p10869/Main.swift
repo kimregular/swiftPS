e59df0117e9bfb36b6bf64c4d071b0eedc78ce23
kimregular <regular.jk.kim@gmail.com>
2024-11-01 23:21:26 +0900

    BJ : https://www.acmicpc.net/problem/10869

---
+++ p10869/Main.swift
@@ -0,0 +1,11 @@
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
