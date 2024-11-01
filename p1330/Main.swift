09fa6ddf8ff62712c2b1a3e366e54680bb309dc9
kimregular <regular.jk.kim@gmail.com>
2024-11-01 23:27:58 +0900

    BJ : https://www.acmicpc.net/problem/1330

--- p1330/Main.swift
+++ p1330/Main.swift
@@ -1,8 +1,14 @@
 let input = readLine()!
+
 let inputArr = input.split(separator: " ")
 
 let a = Int(inputArr[0])!
 let b = Int(inputArr[1])!
-let c = Int(inputArr[2])!
 
-print(a + b + c)
+if a > b {
+    print(">")
+} else if a < b {
+    print("<")
+} else {
+    print("==")
+}
